d4479f23550a7643ddbd1347b9bd941e9dd23e9b
Michael Wanyoike <micwanyoike@gmail.com>
2018-10-22 17:35:18 +0300

    Defined Product Component

---
+++ src/components/Product.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="product">
+    <b-col>
+      <b-card class="text-centers" :title="product.name">
+        <ul>
+          <li><span class="bold">Stock : </span>{{product.stock}}</li>
+          <li><span class="bold">Price : </span>{{product.price}} </li>
+        </ul>
+        <b-button variant="info">Add to Cart</b-button>
+      </b-card>
+    </b-col>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'Product',
+    props: ['product']
+  }
+</script>
+
+<style scoped>
+  button {
+    float: right;
+  }
+  .bold {
+    font-weight: bold;
+  }
+</style>
--- src/components/ProductList.vue
+++ src/components/ProductList.vue
@@ -1,53 +1,33 @@
 <template>
   <div class="product-list">
     <b-row>
-      <b-col>
-        <b-card class="text-centers"  title="Samsung Galaxy S9">
-          <ul>
-            <li><span class="bold">Stock : </span>25</li>
-            <li><span class="bold">Price : </span>$1,000</li>
-          </ul>
-          <b-button variant="info">Add to Cart</b-button>
-        </b-card>
-      </b-col>
-      <b-col>
-        <b-card class="text-centers"  title="Xiaomi Explorer">
-          <ul>
-            <li><span class="bold">Stock : </span>25</li>
-            <li><span class="bold">Price : </span>$1,000</li>
-          </ul>
-          <b-button variant="info">Add to Cart</b-button>
-        </b-card>
-      </b-col>
-      <b-col>
-        <b-card class="text-centers"  title="Oppo Find X">
-          <ul>
-            <li><span class="bold">Stock : </span>25</li>
-            <li><span class="bold">Price : </span>$1,000</li>
-          </ul>
-          <b-button variant="info">Add to Cart</b-button>
-        </b-card>
-      </b-col>
+      <Product v-bind:product="product"/>
     </b-row>
-
   </div>
 </template>
 
 <script>
-export default {
-  name: 'ProductList',
-  props: { }
-}
+  import Product from './Product.vue'
+
+  export default {
+    name: 'ProductList',
+    components: {
+      Product
+    },
+    data() {
+      return {
+        product: {
+          name:"Samsung Galaxy S9",
+          stock: 16,
+          price: "$707.99"
+        }
+      }
+    }
+  }
 </script>
 
 <style scoped>
-.product-list {
-  margin-top: 25px;
-}
-button {
-  float: right;
-}
-.bold {
-  font-weight: bold;
-}
+  .product-list {
+    margin-top: 25px;
+  }
 </style>

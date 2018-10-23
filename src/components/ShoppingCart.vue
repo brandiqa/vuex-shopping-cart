7cce7edb0062cf796362f3fe2614fb868891502b
Michael Wanyoike <micwanyoike@gmail.com>
2018-10-23 12:39:50 +0300

    Implemented Shopping Cart static UI

--- src/components/ShoppingCart.vue
+++ src/components/ShoppingCart.vue
@@ -1,9 +1,32 @@
 <template>
-  <p>Shopping Cart code missing</p>
+  <div class="shopping-cart">
+    <b-table striped hover head-variant="dark" :items="items"></b-table>
+  </div>
 </template>
 
 <script>
+  const items = [
+    { product: 'Pocophone FI', quantity: 2, price: "$200", cost: "$400" },
+    { product: 'Samsung S9', quantity: 2, price: "$200", cost: "$900" },
+    { price: "TOTAL:", cost: "$1300"}
+  ]
+
   export default {
-    name: 'ShoppingCart'
+    name: 'ShoppingCart',
+    data () {
+      return {
+        items: items
+      }
+    }
   }
 </script>
+
+<style>
+  table {
+    margin-top: 24px;
+  }
+  .shopping-cart tr:last-child {
+    font-weight: bold !important;
+    color: cornflowerblue;
+  }
+</style>

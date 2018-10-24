29ea3810a3114cd5b2408d81206492845d475eaf
Michael Wanyoike <micwanyoike@gmail.com>
2018-10-24 11:48:31 +0300

    Defined OrderNo

--- src/components/OrderList.vue
+++ src/components/OrderList.vue
@@ -1,18 +1,23 @@
 <template>
   <div class="order-list">
-    <b-table striped hover head-variant="dark" :items="items"></b-table>
+    <b-table striped hover head-variant="dark" :fields="fields" :items="items">
+      <template slot="orderNo" slot-scope="data">
+        #00{{ data.index + 1}}
+      </template>
+    </b-table>
   </div>
 </template>
 
 <script>
+  const fields = ["orderNo", "products", "totalCost"]
   const items = [
-    { orderDate: "2018-10-16", products: "2x Pocophone @ $350, 1x Samsung S9 @ $700", total: "$1,300" },
+    { products: "2x Pocophone @ $350, 1x Samsung S9 @ $700", totalCost: "$1,300" },
   ]
 
   export default {
     data () {
       return {
-        items: items
+        fields, items
       }
     }
   }
